99c53d7dd1ccc4eb83f1d6f9284065502978d339
Z Zhang <ughwuyeguaehsakhasiklazdcaabulakhabuljiha@AndrewZs-MacBook-Air.local>
2023-02-04 15:12:59 -0800

    update search function based on start_date and end_date range

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,19 @@
 // to run: go to `~/.aws/credentials` and put in the credentials commented out here
 
 const express = require("express");
-const path = require("path"); 
-const bodyParser = require("body-parser"); 
-const express = require("express"); 
-const cors = require("cors"); 
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const axios = require("axios")
-const http = require("http"); 
-const fs = require("fs"); 
+const http = require("http");
+const fs = require("fs");
 
 
 const app = express();
 
 const AWS = require('aws-sdk');
 const AWS_REGION = 'us-west-2';
-const API_KEY = "SGHeOOlwlk4UXftzmjcomj9Zf1hmK2cobZhZJTtH"; 
+const API_KEY = "SGHeOOlwlk4UXftzmjcomj9Zf1hmK2cobZhZJTtH";
 
 
 AWS.config.update({
@@ -28,12 +27,16 @@ console.log('process.cwd(): ', process.cwd());
 console.log('__dirname: ', __dirname);
 
 app.get('/events', (req, res) => {
+  const startDate = req.query.start_date;
+  const endDate = req.query.end_date;
+  console.log(startDate);
   const params = {
     TableName: 'astronomy-events',
-    KeyConditionExpression: 'ID = :partitionKeyValue and DateStart = :sortKeyValue',
+    KeyConditionExpression: 'ID = :partitionKeyValue and (DateStart BETWEEN :startDateSortKeyValue AND :endDateSortKeyValue)',
     ExpressionAttributeValues: {
       ':partitionKeyValue': { S: 'event' },
-      ':sortKeyValue': { N: '20250809' }
+      ':startDateSortKeyValue': { N: startDate },
+      ':endDateSortKeyValue': { N: endDate }
     }
   }
   dynamoDB.query(params, (error, data) => {
@@ -44,7 +47,7 @@ app.get('/events', (req, res) => {
     }
     else{
       console.log(JSON.stringify(data))
-      res.send(JSON.stringify(data));
+      res.status(200).send(JSON.stringify(data));
     }
   });
 
@@ -52,17 +55,17 @@ app.get('/events', (req, res) => {
 
 
 app.get('/images', async (req, res) => {
-  // get images from nasa api 
-  // everyday update one image fron nasa apod-api 
+  // get images from nasa api
+  // everyday update one image fron nasa apod-api
    // https://api.nasa.gov/planetary/apod?api_key=SGHeOOlwlk4UXftzmjcomj9Zf1hmK2cobZhZJTtH
   try {
     const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
     const data = response.data;
     console.log('data: ', data);
-    let hdurl = response.data.hdurl; 
+    let hdurl = response.data.hdurl;
     console.log('hdurl: ', hdurl);
     // res.send(data);
-    res.send(hdurl); 
+    res.send(hdurl);
   } catch (error) {
     console.error(error);
     res.send('Error fetching data from NASA API');
@@ -74,9 +77,9 @@ app.get('/images', async (req, res) => {
 // });
 
 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
-    console.log("This server has just started at 8080!"); 
+    console.log("This server has just started at 8080!");
 })
 
 
