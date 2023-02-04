f5a36f2d9c38f481ee6338f98f46e75309278d6e
Haoyu Guo <haoyuguo@usc.edu>
2023-02-04 14:32:06 -0800

    update backend nasa api images

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,31 @@
 // to run: go to `~/.aws/credentials` and put in the credentials commented out here
 
 const express = require("express");
+const path = require("path"); 
+const bodyParser = require("body-parser"); 
+const express = require("express"); 
+const cors = require("cors"); 
+const axios = require("axios")
+const http = require("http"); 
+const fs = require("fs"); 
+
+
 const app = express();
 
 const AWS = require('aws-sdk');
 const AWS_REGION = 'us-west-2';
+const API_KEY = "SGHeOOlwlk4UXftzmjcomj9Zf1hmK2cobZhZJTtH"; 
+
 
 AWS.config.update({
   region: AWS_REGION,
 });
 const dynamoDB = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
+
+console.log('process.cwd(): ', process.cwd());
+
+console.log('__dirname: ', __dirname);
 
 app.get('/events', (req, res) => {
   const params = {
@@ -35,8 +50,33 @@ app.get('/events', (req, res) => {
 
 });
 
-app.listen(3007, () => {
-  console.log('Example app listening on port 3007!');
+
+app.get('/images', async (req, res) => {
+  // get images from nasa api 
+  // everyday update one image fron nasa apod-api 
+   // https://api.nasa.gov/planetary/apod?api_key=SGHeOOlwlk4UXftzmjcomj9Zf1hmK2cobZhZJTtH
+  try {
+    const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+    const data = response.data;
+    console.log('data: ', data);
+    let hdurl = response.data.hdurl; 
+    console.log('hdurl: ', hdurl);
+    // res.send(data);
+    res.send(hdurl); 
+  } catch (error) {
+    console.error(error);
+    res.send('Error fetching data from NASA API');
+  }
 });
+
+// app.listen(3007, () => {
+//   console.log('Example app listening on port 3007!');
+// });
+
+
+const port = process.env.PORT || 8080; 
+app.listen(port, function () {
+    console.log("This server has just started at 8080!"); 
+})
 
 
